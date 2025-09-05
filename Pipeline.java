3d43f050d4b3f80d354a86bd0f03a607bed19c83
Jayasuriya-S-S <jais.tech87@gmail.com>
2025-09-05 17:17:48 +0530

    Create Pipeline.java

---
+++ Pipeline.java
@@ -0,0 +1,26 @@
+package devops;
+
+class Jenkins{
+	Jenkins(){
+		System.out.println("welcome to jenkins");
+	}
+}
+class ci extends Jenkins{
+	ci(){
+		System.out.println("Ci process start");
+	}
+}
+
+public class Pipeline extends ci {
+void line(){
+
+	System.out.println("executed pipeline");
+	
+}
+	public static void main(String[] args) {
+		Pipeline execute = new Pipeline();
+		
+       execute.line();
+	}
+
+}
